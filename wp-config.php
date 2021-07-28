51113dc782ec22ebfe29300fae0a227ee8ad95ae
rsamolinski <88008164+rsamolinski@users.noreply.github.com>
2021-07-28 14:56:58 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -3,10 +3,10 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'rsamolinski' );
+define( 'DB_USER', 'wordpress' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Fullsail1' );
+define( 'DB_PASSWORD', 'Fullsail1!' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
